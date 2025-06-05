677b0f787f4cc878dd79ebd4ae25d7cc78c58138
Screw_Driver <120485741+LachezarOrNot@users.noreply.github.com>
2025-06-05 08:58:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        string number = Console.ReadLine();
+        int fromBase = int.Parse(Console.ReadLine());//br sistema
+        int toBase = int.Parse(Console.ReadLine());//kum koq broina
+        string converted = Convert.ToString(Convert.ToInt32(number, fromBase), toBase);
+        Console.WriteLine(converted);
+                   
+    }
+}
